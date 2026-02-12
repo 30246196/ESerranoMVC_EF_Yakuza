1ada7ce7a6e093eac97e5fbfead49c4ecb4b2402
30246196 <edithserra1@gmail.com>
2026-02-12 22:51:40 +0000

    Step 6: Added the DbContext Initializer to manage automatic DB recreation. - Created YakuzaDbInitializer.cs (custom initializer using DropCreateDatabaseIfModelChanges) - Updated Global.asax to register the initializer in Application_Start()

--- ESerranoMVC_EF_Yakuza/Global.asax.cs
+++ ESerranoMVC_EF_Yakuza/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ESerranoMVC_EF_Yakuza.Models;
 
 namespace ESerranoMVC_EF_Yakuza
 {
@@ -12,6 +14,9 @@ namespace ESerranoMVC_EF_Yakuza
     {
         protected void Application_Start()
         {
+            // added in Step 6: add the DBcontext initialising Strategy
+            Database.SetInitializer(new YakuzaDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ ESerranoMVC_EF_Yakuza/Models/YakuzaDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ESerranoMVC_EF_Yakuza.Models
+{
+    public class YakuzaDbInitializer: DropCreateDatabaseIfModelChanges<YakuzaContext>
+    {
+        // If the model changes->drop the DB-> recreate it
+        protected override void Seed(YakuzaContext context)
+        {
+            // Step 7 : Seed the Database
+            base.Seed(context);
+        }
+    }
+}
